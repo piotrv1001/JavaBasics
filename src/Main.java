9e458018aea758b4614dfc1cd2576b6976ecce3d
piotrv1001 <piotrv1001@interia.pl>
2022-05-20 13:01:07 +0200

    some basics

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Arrays;
+
 public class Main {
+
+    static int sum(int a, int b){
+        return a + b;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+//        int a = 5;
+//        int b = 6;
+//        int sum = sum(5, 6);
+//        System.out.println(a + " + " + b + " = " + (5+6));
+//        System.out.println(a + " + " + b + " = " + sum(a, b));
+//        char a = 97;
+//        System.out.println(a);
+        String str = "Hello world";
+        String[] strings = str.split(" ");
+        for(int i = 0; i < strings.length; i++){
+            System.out.println("Word " + (i + 1) + ": " + strings[i]);
+        }
+        int[] nums = {1, 2, 3, 4, 5};
+        for(int num : nums){
+            System.out.println(num);
+        }
     }
 }
