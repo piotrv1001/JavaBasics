227f20524efbba5ed504ae50ce7f453e9e7ff235
piotrv1001 <piotrv1001@interia.pl>
2022-05-20 15:11:04 +0200

    more changes

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,33 @@
 import java.util.Arrays;
+import java.util.Scanner;
+
+class Point{
+
+    private int x = 0;
+    private int y = 0;
+
+    public Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX(){
+        return this.x;
+    }
+
+    public int getY(){
+        return this.y;
+    }
+
+    public void setX(int newX){
+        this.x = newX;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
+
 
 public class Main {
 
@@ -22,6 +51,35 @@ public class Main {
         int[] nums = {1, 2, 3, 4, 5};
         for(int num : nums){
             System.out.println(num);
+        }
+        float x = 10.0f;
+        float y = 3.0f;
+        float result = x / y;
+        float rounded = Math.round(result * 100.0f) / 100.0f;
+        System.out.println(rounded);
+
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Give me a number");
+//        int number = input.nextInt();
+//        System.out.println("You entered: " + number);
+//        System.out.println("Give me your name");
+//        String name = input.next();
+//        System.out.println("Your name is: " + name);
+//        input.close();
+
+        Point p1 = new Point(10, 10);
+        // Point p2 = new Point();
+        p1.setX(5);
+        p1.setY(4);
+        System.out.println("X = " + p1.getX());
+        System.out.println("Y = " + p1.getY());
+
+        String str1 = "Hello world";
+        String lowerStr1 = str1.toLowerCase();
+        if (str1.equals(lowerStr1)){
+            System.out.println(str1 + " is all lowercase");
+        }else{
+            System.out.println(str1 + " is not all lowercase");
         }
     }
 }
